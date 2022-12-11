96b6ffc79e4a79761d7fa227be1d039f54f5203b
LeandroShift <leandromendesluiz@hotmail.com>
2022-12-11 13:06:56 -0300

    pokedex

---
+++ assets/js/poke-api.js
@@ -0,0 +1,39 @@
+
+
+const pokeAPi = {}
+
+function convertPOkeApiDetailtoPokemon(pokeDetail){
+    const pokemon = new Pokemon ()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeAPi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPOkeApiDetailtoPokemon)
+}
+
+
+pokeAPi.getPokemons = (offset = 0, limit = 20) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPi.getPokemonDetail))
+        .then((detailResquests) => Promise.all(detailResquests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
